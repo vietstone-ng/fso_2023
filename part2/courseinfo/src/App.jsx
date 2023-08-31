df0d7f0e9adec64ee78d36c4d4fb97d344029450
Viet Nguyen Tran <vietstone.ng@gmail.com>
2023-08-31 17:43:55 +0700

    exercise 2.3

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,70 @@
+const Header = (props) => {
+  return <h1>{props.course}</h1>
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+}
+
+const Content = (props) => {
+  const parts = props.parts.map((part) => {
+    return <Part key={part.id} part={part.name} exercises={part.exercises} />
+  })
+
+  return <>{parts}</>
+}
+
+const Total = (props) => {
+  return <b>total of {props.exercises} exercises</b>
+}
+
+const Course = (props) => {
+  return (
+    <>
+      <Header course={props.course.name} />
+      <Content parts={props.course.parts} />
+      <Total
+        exercises={props.course.parts
+          .map((part) => part.exercises)
+          .reduce((x, y) => x + y)}
+      />
+    </>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3,
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4,
+      },
+    ],
+  }
+
+  return <Course course={course} />
+}
+
+export default App
