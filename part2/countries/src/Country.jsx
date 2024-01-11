47c7b54f1b4791bacbd54ef8f1fcf65a4f2bcef5
Viet Nguyen Tran <vietstone.ng@gmail.com>
2024-01-11 14:45:48 +0700

    exercise 2.20

--- part2/countries/src/Country.jsx
+++ part2/countries/src/Country.jsx
@@ -1,3 +1,6 @@
+import { useEffect } from 'react'
+import { useState } from 'react'
+
 function Country({ country }) {
   if (!country) {
     return null
@@ -8,14 +11,48 @@ function Country({ country }) {
       <h1>{country.name.common}</h1>
       <div>capital {country.capital[0]}</div>
       <div>area {country.area}</div>
-      <h2>languages:</h2>
+      <h3>languages:</h3>
       <ul>
         {Object.values(country.languages).map((language) => (
           <li key={language}>{language}</li>
         ))}
       </ul>
       <img src={country.flags.png} alt={country.name.common} width='150px' />
+
+      <Weather country={country} />
     </div>
+  )
+}
+
+function Weather({ country }) {
+  const [weather, setWeather] = useState(null)
+
+  const capital = country.capital[0]
+
+  useEffect(() => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?appid=${
+        import.meta.env.VITE_SOME_KEY
+      }&q=${capital}&units=metric`
+    )
+      .then((response) => response.json())
+      .then((data) => setWeather(data))
+  }, [capital])
+
+  if (!weather) {
+    return null
+  }
+
+  return (
+    <>
+      <h2>Weather in {country.capital[0]}</h2>
+      <div>temperature {weather.main.temp} Celcius</div>
+      <img
+        src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+        alt={weather.weather[0].description}
+      />
+      <div>wind: {weather.wind.speed} m/s</div>
+    </>
   )
 }
 
